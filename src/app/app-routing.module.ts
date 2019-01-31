8889f3559936a18adeb6f66b5e8bc3a1b6bf9aa8
bazal4o <todor.paskalev@gmail.com>
2019-01-31 20:09:39 +0200

    Cleanup router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ComponentsComponent } from './components/components.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home',
-    children: [
-      { path: '', component: HomeComponent },
-      { path: ':index', component: HomeComponent }
-    ]
-  },
+  { path: '', redirectTo: '/home', pathMatch: 'full', },
+  { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({
