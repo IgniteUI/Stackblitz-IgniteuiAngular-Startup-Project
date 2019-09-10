55a18f6b147de4a0d35a23ef3aba6ad05f16d618
ViktorSlavov <viktorslavov91@gmail.com>
2019-09-10 18:35:09 +0300

    feat(routing): add tabs routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { routes } from './app-routing.module';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'IgniteUI for Angular';
+  routes = routes.filter(r => r.data);
 }
